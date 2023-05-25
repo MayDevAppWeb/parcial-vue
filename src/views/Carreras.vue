940b000da9b3333b2a1399e0c692eb5f3eb437d3
May Gaviria <may.dev.app.web@gmail.com>
2023-05-24 23:24:32 -0500

    Final Project

---
+++ src/views/Carreras.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container" id="carreras">
+        <h1>Carreras List</h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Carreras</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr tr v-for="(carreras,index) in carreras " :key="index">
+      <th scope="row">{{ index + 1 }}</th>
+      <td>{{carreras.Nom_Car}}</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+    </template>
+    
+    <script>
+    import axios from 'axios'
+    export default{
+    
+        name: 'carreras',
+        data(){
+            return{
+                carreras: []
+            }
+        },
+        mounted() {
+            axios.get('http://127.0.0.1:8000/api/carreras').then(response => {this.carreras = response.data.carreras;
+            });
+        },
+    
+    }
+    
+    </script>
